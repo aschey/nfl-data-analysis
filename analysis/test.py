3d5b31b275df8f6e35caeaf6ade9f2975a9e21e1
aschey <aschey13@gmail.com>
2020-10-18 12:07:32 -0500

    better scores

--- analysis/test.py
+++ analysis/test.py
@@ -22,6 +22,18 @@ vals = [
         {'team1': 7, 'team2': 14, 'q': 2},
         {'team1': 7, 'team2': 21, 'q': 3},
         {'team1': 31, 'team2': 30, 'q': 4}
+    ],
+    [
+        {'team1': 7, 'team2': 6, 'q': 1},
+        {'team1': 21, 'team2': 14, 'q': 2},
+        {'team1': 24, 'team2': 21, 'q': 3},
+        {'team1': 31, 'team2': 30, 'q': 4}
+    ],
+    [
+        {'team1': 7, 'team2': 6, 'q': 1},
+        {'team1': 21, 'team2': 6, 'q': 2},
+        {'team1': 24, 'team2': 6, 'q': 3},
+        {'team1': 31, 'team2': 14, 'q': 4}
     ]
 ]
 
@@ -30,18 +42,19 @@ def f(score_diff, quarter, max_pos_diff, max_neg_diff):
     if score_diff > 0:
         s = score_diff ** (11/10) / (5 - quarter)
         d1 = -(1 - (score_diff/max_pos_diff if max_pos_diff != 0 else 1)) * (5-quarter)
-        d2 = (-max_neg_diff + score_diff) ** 2 / (5 - quarter)
+        d2 = max_neg_diff ** 2 / (5 - quarter)
     else:
         s = - ((-score_diff + (10 * quarter)) ** (11/10) + 10)/100
         d1 = (-max_pos_diff + score_diff) * quarter
         d2 = -(score_diff/max_neg_diff if max_neg_diff != 0 else 1) * (5-quarter)
-    #print(s, d1, d2)
+    print(s, d1, d2)
     return s + d1 + d2
 
 
-for v in vals:
+for i, v in enumerate(vals):
     mp = 0
     mn = 0
+    print(i)
     for val in v:
         x = val['team1'] - val['team2']
         mp = max(mp, x)
