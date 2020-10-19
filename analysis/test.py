5dde04f4fc995b900e7d9ea62f665b6345e40e97
aschey <aschey13@gmail.com>
2020-10-18 22:17:50 -0500

    game score kinda working

--- analysis/test.py
+++ analysis/test.py
@@ -1,9 +1,19 @@
+import math
+from numpy import diff
+from itertools import takewhile
+
 vals = [
     [
         {'team1': 17, 'team2': 3, 'q': 1},
         {'team1': 24, 'team2': 3, 'q': 2},
         {'team1': 24, 'team2': 6, 'q': 3},
         {'team1': 24, 'team2': 30, 'q': 4}
+    ],
+    [
+        {'team1': 17, 'team2': 3, 'q': 1},  # 14
+        {'team1': 24, 'team2': 3, 'q': 2},  # 21 6
+        {'team1': 24, 'team2': 9, 'q': 3},  # 15 -6
+        {'team1': 24, 'team2': 30, 'q': 4}  # -6 -21
     ],
     [
         {'team1': 10, 'team2': 17, 'q': 1},
@@ -24,6 +34,18 @@ vals = [
         {'team1': 31, 'team2': 30, 'q': 4}
     ],
     [
+        {'team1': 3, 'team2': 17, 'q': 1},  # -14
+        {'team1': 3, 'team2': 21, 'q': 2},  # -18 -4
+        {'team1': 7, 'team2': 21, 'q': 3},  # -14 4
+        {'team1': 31, 'team2': 30, 'q': 4}  # 1 15
+    ],
+    [
+        {'team1': 3, 'team2': 17, 'q': 1},  # -14
+        {'team1': 3, 'team2': 17, 'q': 2},  # -14 0
+        {'team1': 7, 'team2': 21, 'q': 3},  # -14 0
+        {'team1': 31, 'team2': 30, 'q': 4}  # 1 15
+    ],
+    [
         {'team1': 7, 'team2': 6, 'q': 1},
         {'team1': 21, 'team2': 14, 'q': 2},
         {'team1': 24, 'team2': 21, 'q': 3},
@@ -35,18 +57,46 @@ vals = [
         {'team1': 24, 'team2': 6, 'q': 3},
         {'team1': 31, 'team2': 14, 'q': 4}
     ]
+
 ]
 
 
-def f(score_diff, quarter, max_pos_diff, max_neg_diff):
-    if score_diff > 0:
-        s = score_diff ** (11/10) / (5 - quarter)
-        d1 = -(1 - (score_diff/max_pos_diff if max_pos_diff != 0 else 1)) * (5-quarter)
-        d2 = max_neg_diff ** 2 / (5 - quarter)
+def sigmoid(x, max_val, growth):
+    dividend = 1 + math.e ** (-growth * (x - math.log(max_val) / growth))
+    return (max_val + 1) / dividend - 1
+
+
+def g(all_diffs, quarter, negative):
+    # get slope of all score diffs (negative slope means decreasing margin and vice versa)
+    derivative = diff(all_diffs)
+    if len(derivative) == 0:
+        return 0
+
+    filter = (lambda v: v < 0) if negative else (lambda v: v > 0)
+    # get last run of increasing or decreasing margin
+    run = list(takewhile(filter, derivative[::-1]))[::-1]
+    # take square root of squared margins, this increases weight of points scored in a single quarter
+    # also weight later quarters higher
+    avg = math.sqrt(sum((v * (i + 1)) ** 2 for i, v in enumerate(run))) / max(len(run), 1)
+    ret = (avg * (quarter / 4)) ** 1.5
+    return -ret if negative else ret
+
+
+def f(score_diff, quarter, max_pos_diff, max_neg_diff, all_diffs):
+    if score_diff >= 0:
+        # current lead
+        s = sigmoid(score_diff, 50, 0.3 + (0.03 * quarter))
+        # current lead relative to max lead
+        d1 = sigmoid(score_diff/max(max_pos_diff, 1), 10 * quarter, 10 * quarter)
+        # maximum deficit overcome
+        d2 = (score_diff - max_neg_diff) + g(all_diffs, quarter, False)
     else:
-        s = - ((-score_diff + (10 * quarter)) ** (11/10) + 10)/100
-        d1 = (-max_pos_diff + score_diff) * quarter
-        d2 = -(score_diff/max_neg_diff if max_neg_diff != 0 else 1) * (5-quarter)
+        # current defecit
+        s = -sigmoid(-score_diff, 50, 0.3 + (0.03 * quarter))
+        # current deficit relative to max deficit
+        d1 = -sigmoid(score_diff/max_neg_diff, 10 * quarter, 10 * quarter)
+        # total deficit from previous max lead
+        d2 = score_diff - max_pos_diff + g(all_diffs, quarter, True)
     print(s, d1, d2)
     return s + d1 + d2
 
@@ -54,10 +104,26 @@ def f(score_diff, quarter, max_pos_diff, max_neg_diff):
 for i, v in enumerate(vals):
     mp = 0
     mn = 0
+    mpq = 1
+    current_q = 1
+    prev_q_diffs = []
+    current_q_diff = 0
     print(i)
-    for val in v:
+    for j, val in enumerate(v):
         x = val['team1'] - val['team2']
         mp = max(mp, x)
         mn = min(mn, x)
-        print(f(x, val['q'], mp, mn))
+
+        if val['q'] > current_q or j == len(v) - 1:
+            current_q = val['q']
+
+            prev_q_diffs.append(current_q_diff)
+            if j == len(v) - 1:
+                prev_q_diffs.append(x)
+            prev_q_diff = current_q_diff
+        current_q_diff = x
+        if val['q'] == 4:
+            pass
+
+        print(f(x, val['q'], mp, mn, prev_q_diffs))
     print()
