59fef63faf48747ebe0f0bca701699c96fb7b6dc
aschey <aschey13@gmail.com>
2021-08-28 16:31:03 -0500

    switch api url

--- nfl-misery-index/util/fetchUtil.ts
+++ nfl-misery-index/util/fetchUtil.ts
@@ -19,7 +19,7 @@ const success = async <T>(response: Response): Promise<T> => {
   return data;
 };
 
-const baseUrl = "https://api.aschey.tech/nfl";
+const baseUrl = "http://localhost:3000/api";
 
 export const getJson = async <T>(url: string): Promise<T> => {
   const response = await fetch(baseUrl + url, { method: "GET", ...options });
