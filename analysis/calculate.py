e8f11947ae8cb6df0b1b878d2349f1fda3856ac2
aschey <aschey13@gmail.com>
2021-09-04 14:26:11 -0500

    Fix time calculation

--- analysis/calculate.py
+++ analysis/calculate.py
@@ -3,6 +3,8 @@ from typing import List, Union
 from numpy import diff
 from itertools import takewhile
 
+QUARTER_MINS = 15.0
+
 
 class Scorer:
     def __init__(self):
@@ -25,11 +27,17 @@ class Scorer:
         diff = team1_score - team2_score
         self.max_pos_diff = max(self.max_pos_diff, diff)
         self.max_neg_diff = min(self.max_neg_diff, diff)
-        total_time = quarter * 15.0
-        if quarter_time_remaining is not None:
-            minutes, seconds = quarter_time_remaining.split(":")
-            quarter_mins = 15.0 - (float(minutes) + float(seconds) / 60)
-            total_time += quarter_mins
+
+        # Default to halfway through the quarter if time is not known
+        if quarter_time_remaining is None:
+            quarter_time_remaining = "7:50"
+
+        minutes, seconds = quarter_time_remaining.split(":")
+        quarter_mins = QUARTER_MINS - (float(minutes) + float(seconds) / 60)
+
+        # If score happened at 15:00 in the first quarter we get 0 for total time
+        # Default to 0.01 in this case to prevent division by zero errors
+        total_time = max((quarter - 1) * QUARTER_MINS + quarter_mins, 0.01)
 
         if quarter > self.current_quarter or final:
             self.current_quarter = quarter
@@ -37,9 +45,9 @@ class Scorer:
             if final:
                 self.previous_quarter_diffs.append(diff)
         self.current_quarter_diff = diff
-        new_score = self.f(
+        new_score = self._calculate_score(
             diff,
-            quarter,
+            total_time,
             self.max_pos_diff,
             self.max_neg_diff,
             self.previous_quarter_diffs,
@@ -47,11 +55,13 @@ class Scorer:
         self.prev_score = new_score
         return new_score
 
-    def sigmoid(self, x: float, max_val: float, growth: float) -> float:
+    def _sigmoid(self, x: float, max_val: float, growth: float) -> float:
         dividend = 1 + math.e ** (-growth * (x - math.log(max_val) / growth))
         return (max_val + 1) / dividend - 1
 
-    def g(self, all_diffs: List[int], total_time: float, multiplier: int) -> float:
+    def _weighted_run(
+        self, all_diffs: List[int], total_time: float, multiplier: int
+    ) -> float:
         # get slope of all score diffs (negative slope means decreasing margin and vice versa)
         derivative = diff(all_diffs)
         if len(derivative) == 0:
@@ -67,7 +77,7 @@ class Scorer:
         ret = (avg * (total_time / max(total_time, 60))) ** 1.5
         return multiplier * ret
 
-    def f(
+    def _calculate_score(
         self,
         score_diff: int,
         total_time: float,
@@ -86,18 +96,19 @@ class Scorer:
             deficit_diff = max_pos_diff
             extreme_func = min
 
-        quarter_time = total_time / 15
+        quarter_time = total_time / QUARTER_MINS
         # current lead or current deficit
-        s = multiplier * self.sigmoid(
+        s = multiplier * self._sigmoid(
             multiplier * score_diff, 50, 0.3 + (0.03 * quarter_time)
         )
         # current lead relative to max lead or current deficit relative to max deficit
-        d1 = multiplier * self.sigmoid(
+        d1 = multiplier * self._sigmoid(
             score_diff / extreme_func(lead_diff, multiplier),
             10 * quarter_time,
             10 * quarter_time,
         )
         # maximum deficit overcome (if winning) or total deficit from previous max lead (if losing)
-        d2 = score_diff - deficit_diff + self.g(all_diffs, total_time, multiplier)
+        run = self._weighted_run(all_diffs, total_time, multiplier)
+        d2 = score_diff - deficit_diff + run
 
         return s + d1 + d2
